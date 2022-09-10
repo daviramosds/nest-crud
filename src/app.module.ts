a317dd70cbbd7a89276a04842a6bb988905cc2f6
daviramosds <davirds.dev@gmail.com>
2022-09-10 11:31:02 -0300

    add prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { User2Module } from './user2/user2.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, User2Module],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  async create(@Body() createUserDTO: CreateUserDTO) {
+    return {
+      message: 'user created',
+    };
+  }
 }
